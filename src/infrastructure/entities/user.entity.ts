c4b5b8d826e7023dbe072f0eb26eff4f0e854e90
sonchaegeon <thscorjs1002@naver.com>
2021-12-28 11:14:43 +0900

    ⚡️ :: (#1) user entity

---
+++ src/infrastructure/entities/user.entity.ts
@@ -0,0 +1,19 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ length: 10 })
+  provider: string;
+
+  @Column({ length: 320 })
+  email: string;
+
+  @Column({ length: 2000 })
+  profile: string;
+
+  @Column({ length: 30 })
+  name: string;
+}
