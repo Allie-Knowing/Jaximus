17a8259a8fa1475405ed1de7e59830f1fdea1e11
sonchaegeon <thscorjs1002@naver.com>
2021-12-28 11:14:36 +0900

    ⚡️ :: (#1) typeorm connection module

---
+++ src/infrastructure/config/typeorm/typeorm.module.ts
@@ -0,0 +1,33 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { EnvironmentConfigModule } from '../environment-config/environment-config.module';
+import { EnvironmentConfigService } from '../environment-config/environment-config.service';
+
+export const getTypeOrmModuleOptions = (
+  config: EnvironmentConfigService,
+): TypeOrmModuleOptions =>
+  ({
+    type: 'mysql',
+    host: config.getDatabaseHost(),
+    port: config.getDatabasePort(),
+    username: config.getDatabaseUser(),
+    password: config.getDatabasePassword(),
+    database: config.getDatabaseName(),
+    synchronize: config.getDatabaseSync(),
+    logging:
+      process.env.NODE_ENV === 'local' || process.env.NODE_ENV === 'test'
+        ? true
+        : false,
+    entities: [__dirname + './../../**/*.entity{.ts,.js}'],
+  } as TypeOrmModuleOptions);
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [EnvironmentConfigModule],
+      inject: [EnvironmentConfigService],
+      useFactory: getTypeOrmModuleOptions,
+    }),
+  ],
+})
+export class TypeOrmConfigModule {}
