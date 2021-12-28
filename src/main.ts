14ef6c5aa1278a3564f8b64a1044b402706e6312
sonchaegeon <thscorjs1002@naver.com>
2021-12-28 11:16:40 +0900

    ⚡️ :: (#1) bootstrap method
    global filters, global interceptors

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { AllExceptionFilter } from './infrastructure/common/filter/exception.filter';
+import { LoggingInterceptor } from './infrastructure/common/interceptors/logger.interceptor';
+import { ResponseInterceptor } from './infrastructure/common/interceptors/response.interceptor';
+import { LoggerService } from './infrastructure/logger/logger.service';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.useGlobalFilters(new AllExceptionFilter(new LoggerService()));
+  app.useGlobalInterceptors(new LoggingInterceptor(new LoggerService()));
+  app.useGlobalInterceptors(new ResponseInterceptor());
+  await app.listen(3000);
+}
+bootstrap();
