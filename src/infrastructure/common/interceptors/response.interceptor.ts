6f0200564395162a0bc81d21e4c737b28001ce88
sonchaegeon <thscorjs1002@naver.com>
2021-12-28 11:13:50 +0900

    ⚡️ :: (#1) response interceptor

---
+++ src/infrastructure/common/interceptors/response.interceptor.ts
@@ -0,0 +1,40 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+export class ResponseFormat<T> {
+  path: string;
+  duration: string;
+  method: string;
+
+  data: T;
+}
+
+@Injectable()
+export class ResponseInterceptor<T>
+  implements NestInterceptor<T, ResponseFormat<T>>
+{
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler,
+  ): Observable<ResponseFormat<T>> {
+    const now = Date.now();
+    const httpContext = context.switchToHttp();
+    const request = httpContext.getRequest();
+
+    return next.handle().pipe(
+      map((data) => ({
+        data,
+        isArray: Array.isArray(data),
+        path: request.path,
+        duration: `${Date.now() - now}ms`,
+        method: request.method,
+      })),
+    );
+  }
+}
