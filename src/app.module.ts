318d20a711f09e19c7ef7b3246fe8ab549427d3e
정지우 <jiwoourty@gmail.com>
2022-02-15 01:20:27 +0900

    :bookmark_tabs: :: (#6) video controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,18 @@ import { Module } from '@nestjs/common';
 import { LoggerModule } from './infrastructure/logger/logger.module';
 import { ExceptionsModule } from './infrastructure/exceptions/exceptions.module';
 import { RepositoriesModule } from './infrastructure/repositories/repositories.module';
-import { UsecasesProxyModule } from './infrastructure/usecases-proxy/usecases-proxy.module';
+import { UsecasesProxyDynamicModule } from './infrastructure/usecases-proxy/usecases-proxy.module';
+import { VideoController } from './presentation/video.controller';
 
 @Module({
   imports: [
     LoggerModule,
     ExceptionsModule,
     RepositoriesModule,
-    UsecasesProxyModule,
+    UsecasesProxyDynamicModule.register(),
   ],
+  controllers: [
+    VideoController,
+  ]
 })
 export class AppModule {}
