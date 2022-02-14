29b1a9360bf213659de059d3a312ef9ce31230b2
정지우 <jiwoourty@gmail.com>
2022-02-15 01:20:10 +0900

    ⚡️ :: (#6) usecase proxy 등록

--- src/infrastructure/usecases-proxy/usecases-proxy.module.ts
+++ src/infrastructure/usecases-proxy/usecases-proxy.module.ts
@@ -1,9 +1,26 @@
-import { Module } from '@nestjs/common';
+import { DynamicModule, Module } from '@nestjs/common';
+import { CreateVideoUsecase } from 'src/usecase/video/create-video';
 import { ExceptionsModule } from '../exceptions/exceptions.module';
 import { LoggerModule } from '../logger/logger.module';
 import { RepositoriesModule } from '../repositories/repositories.module';
+import { DatabaseVideoRepository } from '../repositories/video.repository';
 
 @Module({
   imports: [LoggerModule, RepositoriesModule, ExceptionsModule],
 })
-export class UsecasesProxyModule {}
+export class UsecasesProxyDynamicModule {
+
+  static register(): DynamicModule {
+    return {
+      module: UsecasesProxyDynamicModule,
+      providers: [
+        {
+          inject: [DatabaseVideoRepository],
+          provide: CreateVideoUsecase,
+          useFactory: (databaseVideoRepository: DatabaseVideoRepository) => new CreateVideoUsecase(databaseVideoRepository),
+        }
+      ],
+      exports: [CreateVideoUsecase],
+    };
+  }
+}
