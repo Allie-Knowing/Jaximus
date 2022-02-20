7040c841f6c19cc3690ff3398ccd701209607ce1
sonchaegeon <thscorjs1002@naver.com>
2022-02-20 22:14:59 +0900

    ⚡️ :: (#7) get question list query 구현

--- src/infrastructure/repositories/video.repository.ts
+++ src/infrastructure/repositories/video.repository.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Video } from 'src/domain/model/video';
 import { VideoRepository } from 'src/domain/repositories/video.repository';
 import { Repository } from 'typeorm';
+import { GetQuestionListPresenter } from '../controllers/video/video.presenter';
 import { HashTagTypeOrmEntity } from '../entities/hash-tag.entity';
 import { UserTypeOrmEntity } from '../entities/user.entity';
 import { VideoTypeOrmEntity } from '../entities/video.entity';
@@ -20,16 +21,42 @@ export class DatabaseVideoRepository implements VideoRepository {
     private readonly hashTagEntityRepository: Repository<HashTagTypeOrmEntity>,
   ) {}
 
-  async save(video: Video): Promise<void> {
-      const user: UserTypeOrmEntity = await this.userEntityRepository.findOne(video.userId);
+  getQuestionList(): Promise<GetQuestionListPresenter[]> {
+    return this.videoEntityRepository
+      .createQueryBuilder('video')
+      .leftJoin('video.comments', 'comment')
+      .leftJoin('video.likes', 'like')
+      .innerJoin('video.user', 'user')
+      .select('video.id', 'video_id')
+      .addSelect('video.video_url', 'video_url')
+      .addSelect('video.title', 'title')
+      .addSelect('video.description', 'description')
+      .addSelect('video.created_at', 'created_at')
+      .addSelect('user.id', 'user_id')
+      .addSelect('user.profile', 'profile')
+      .addSelect('COUNT(comment.id)', 'comment_cnt')
+      .addSelect('COUNT(like.id)', 'like_cnt')
+      .groupBy('video.id')
+      .getRawMany();
+  }
 
-      const videoEntity: VideoTypeOrmEntity = await this.videoEntityRepository.save({
+  async save(video: Video): Promise<void> {
+    const user: UserTypeOrmEntity = await this.userEntityRepository.findOne(
+      video.userId,
+    );
+
+    const videoEntity: VideoTypeOrmEntity =
+      await this.videoEntityRepository.save({
         description: video.description,
         title: video.title,
         videoUrl: video.videoUrl,
-        user
+        user,
       });
 
-      await Promise.all(video.hashTags.map(title => this.hashTagEntityRepository.save({ title, question: videoEntity })));
-    }
+    await Promise.all(
+      video.hashTags.map((title) =>
+        this.hashTagEntityRepository.save({ title, question: videoEntity }),
+      ),
+    );
+  }
 }
