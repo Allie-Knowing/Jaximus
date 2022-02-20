93c70f918b3ce9f8569da287385f07d1ef28e99b
sonchaegeon <thscorjs1002@naver.com>
2022-02-20 22:16:07 +0900

    ⚡️ :: (#7) get question list use case 구현

---
+++ src/usecase/video/get-question-list.usecases.ts
@@ -0,0 +1,17 @@
+import { IException } from 'src/domain/exceptions/exceptions.interface';
+import { VideoRepository } from 'src/domain/repositories/video.repository';
+import { GetQuestionListPresenter } from 'src/infrastructure/controllers/video/video.presenter';
+
+export class GetQuestionListUseCases {
+  constructor(
+    private readonly videoRepository: VideoRepository,
+    private readonly exceptionsService: IException,
+  ) {}
+
+  async execute(): Promise<GetQuestionListPresenter[]> {
+    const questions = await this.videoRepository.getQuestionList();
+    if (questions.length === 0)
+      this.exceptionsService.questionNotFoundException();
+    return questions;
+  }
+}
