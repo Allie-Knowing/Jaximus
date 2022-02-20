a7a79289cd0fcc5d386db61b7379f14dfdba923e
Seung jun <poodle2405@gmail.com>
2022-02-20 22:57:18 +0900

    ⚡️ :: (#9) import controllers module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,15 +3,17 @@ import { LoggerModule } from './infrastructure/logger/logger.module';
 import { ExceptionsModule } from './infrastructure/exceptions/exceptions.module';
 import { RepositoriesModule } from './infrastructure/repositories/repositories.module';
 import { UsecasesProxyDynamicModule } from './infrastructure/usecases-proxy/usecases-proxy.module';
-import { VideoController } from './infrastructure/controller/video/video.controller';
+import { VideoController } from './infrastructure/controllers/video/video.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './infrastructure/common/strategies/jwt.strategy';
+import { ControllersModule } from './infrastructure/controllers/controllers.module';
 
 @Module({
   imports: [
     LoggerModule,
     ExceptionsModule,
     RepositoriesModule,
+    ControllersModule,
     UsecasesProxyDynamicModule.register(),
     JwtModule.register({}),
   ],
