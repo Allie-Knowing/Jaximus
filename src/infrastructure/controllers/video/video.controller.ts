967cf7570130b02cd691d622c7c3872d3ea9af87
Seung jun <poodle2405@gmail.com>
2022-03-16 20:18:07 +0900

    ⚡️ :: (#9) video controller

--- src/infrastructure/controllers/video/video.controller.ts
+++ src/infrastructure/controllers/video/video.controller.ts
@@ -5,24 +5,29 @@ import {
   Injectable,
   Post,
   Scope,
+  UploadedFile,
+  UseInterceptors,
 } from '@nestjs/common';
 import { REQUEST } from '@nestjs/core';
 import { Request } from 'express';
 import { Video } from 'src/domain/model/video';
 import { UseCaseProxy } from 'src/infrastructure/usecases-proxy/usecases-proxy';
 import { UsecasesProxyDynamicModule } from 'src/infrastructure/usecases-proxy/usecases-proxy.module';
-import { CreateVideoQuestionUsecase } from 'src/usecase/video/create-video-question.usecase';
-import { CreateVideoCommentUsecase } from 'src/usecase/video/create-video-comment.usecase';
+import { CreateVideoQuestionUsecase } from 'src/usecase/video/create-video-question';
+import { CreateVideoCommentUsecase } from 'src/usecase/video/create-video-comment';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { MulterConfigService } from 'src/infrastructure/config/multer/multer-config.service';
 
-@Injectable({ scope: Scope.REQUEST })
-@Controller('/video')
+@Controller({ path: '/video', scope: Scope.REQUEST })
 export class VideoController {
   constructor(
     @Inject(UsecasesProxyDynamicModule.POST_VIDEO_QUESTION_USECASE_PROXY)
     private readonly postVideoQuestionUsecaseProxy: UseCaseProxy<CreateVideoQuestionUsecase>,
     @Inject(UsecasesProxyDynamicModule.POST_VIDEO_COMMENT_USECASES_PROXY)
     private readonly postVideoCommentUsecaseProxy: UseCaseProxy<CreateVideoCommentUsecase>,
-    @Inject(REQUEST) private readonly request: Request,
+    @Inject(REQUEST)
+    private readonly request: Request,
+    private readonly multerService: MulterConfigService,
   ) {}
 
   @Post('/')
@@ -33,7 +38,15 @@ export class VideoController {
   }
 
   @Post('/answer')
-  createVideoComment(userId: number, @Body() request: Video) {
-    this.postVideoCommentUsecaseProxy.getInstance().execute(userId, request);
+  @UseInterceptors(FileInterceptor('file'))
+  async createVideoComment(
+    @UploadedFile() file,
+    userId: number,
+    @Body() request: Video,
+  ) {
+    await this.postVideoCommentUsecaseProxy
+      .getInstance()
+      .execute(userId, request);
+    return { status: 201, message: 'success' };
   }
 }
