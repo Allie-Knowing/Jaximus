93eb48fa258a81e48e44ef0103b0357b59a81a99
Seung jun <poodle2405@gmail.com>
2022-03-17 22:50:13 +0900

    ⚡️ :: (#10) /video/answer/{video-id}

--- src/presentation/video/video.controller.ts
+++ src/presentation/video/video.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Get, Inject, Post } from '@nestjs/common';
+import { Body, Controller, Get, Inject, Param, Post } from '@nestjs/common';
 import { Video } from 'src/domain/model/video';
 import { CreateVideoUsecase } from 'src/usecase/video/create-video';
 import { GetQuestionListUseCases } from 'src/usecase/video/get-questions-list';
-import { GetQuestionListPresenter } from './video.presenter';
+import { GetVideoCommentListUseCases } from 'src/usecase/video/get-video-comment-list';
+import { GetQuestionListPresenter, GetVideoCommentListPresenter } from './video.presenter';
 
 @Controller('/video')
 export class VideoController {
@@ -11,6 +12,8 @@ export class VideoController {
     private readonly createVideoUsecase: CreateVideoUsecase,
     @Inject(GetQuestionListUseCases)
     private readonly getQuestionListUsecase: GetQuestionListUseCases,
+    @Inject(GetVideoCommentListUseCases)
+    private readonly getVideoCommentListUseCases: GetVideoCommentListUseCases,
   ) {}
 
   @Post('/')
@@ -21,5 +24,10 @@ export class VideoController {
   @Get('/')
   async getQuestionList(): Promise<GetQuestionListPresenter[]> {
     return this.getQuestionListUsecase.execute();
+  }
+
+  @Get('/answer/:video_id')
+  async getVideoCommentList(@Param('video_id') video_id: number): Promise<GetVideoCommentListPresenter[]> {
+    return this.getVideoCommentListUseCases.execute(video_id);
   }
 }
