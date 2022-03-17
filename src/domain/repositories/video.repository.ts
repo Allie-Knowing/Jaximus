e75603ab78f0b3b34a22531a6df3f76a7fa85692
Seung jun <poodle2405@gmail.com>
2022-03-17 23:15:14 +0900

    ⚡️ :: (#10) domain video repository

--- src/domain/repositories/video.repository.ts
+++ src/domain/repositories/video.repository.ts
@@ -1,7 +1,9 @@
 import { GetQuestionListPresenter } from 'src/presentation/video/video.presenter';
 import { Video } from '../model/video';
+import { GetVideoCommentList } from './dto/video.dto';
 
 export interface VideoRepository {
   save(video: Video): Promise<void>;
   getQuestionList(): Promise<GetQuestionListPresenter[]>;
+  getVideoCommentList(video_id: number): Promise<GetVideoCommentList[]>;
 }
