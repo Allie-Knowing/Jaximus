7c9edc018af87739746e2060e31d0b02af0bb3b9
Seung jun <poodle2405@gmail.com>
2022-03-18 15:14:58 +0900

    :recycle: :: (#10) delete validation

--- src/domain/repositories/dto/video.dto.ts
+++ src/domain/repositories/dto/video.dto.ts
@@ -1,18 +1,9 @@
-import { IsDate, IsNumber, IsString } from 'class-validator';
-
 export class GetVideoCommentList {
-  @IsNumber()
   video_id: number;
-  @IsString()
   video_url: string;
-  @IsString()
   title: string;
-  @IsDate()
   created_at: Date;
-  @IsNumber()
   like_cnt: number;
-  @IsString()
   profile: string;
-  @IsNumber()
   user_id: number;
 }
