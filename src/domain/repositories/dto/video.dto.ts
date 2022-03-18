29eedd4ecfe58dbddc9ef582b0f2ce49448e7752
Seung jun <poodle2405@gmail.com>
2022-03-18 17:58:39 +0900

    :recycle: :: code format

--- src/domain/repositories/dto/video.dto.ts
+++ src/domain/repositories/dto/video.dto.ts
@@ -9,13 +9,13 @@ export class GetVideoCommentList {
 }
 
 export class QuestionList {
-    video_id: number;
-    video_url: string;
-    title: string;
-    description: string;
-    like_cnt: number;
-    comment_cnt: number;
-    created_at: Date;
-    profile: string;
-    user_id: number;
+  video_id: number;
+  video_url: string;
+  title: string;
+  description: string;
+  like_cnt: number;
+  comment_cnt: number;
+  created_at: Date;
+  profile: string;
+  user_id: number;
 }
