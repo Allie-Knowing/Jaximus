84a6433b441e570396f823089d7fd9eb05a41d07
Seung jun <poodle2405@gmail.com>
2022-03-18 22:44:24 +0900

    :bookmark_tabs: :: (#16) LikeController 생성

---
+++ src/presentation/likfe/like.controller.ts
@@ -0,0 +1,12 @@
+import { Body, Controller, Inject, Post, Scope } from '@nestjs/common';
+import { CreateLikeUsecase } from 'src/usecase/like/create-like';
+
+@Controller('/like')
+export class LikeController {
+  constructor(@Inject(CreateLikeUsecase) private readonly createLikeUsecase: CreateLikeUsecase) {}
+
+  @Post('/')
+  async createLike(@Body() videoId: number) {
+    this.createLikeUsecase.execute(videoId);
+  }
+}
