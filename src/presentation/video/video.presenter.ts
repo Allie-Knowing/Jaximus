1f8460cbe38277e546150e184ef63fbacedb2e16
Seung jun <poodle2405@gmail.com>
2022-03-18 14:54:17 +0900

    ⚡️ :: (#10) presenter class validator

--- src/presentation/video/video.presenter.ts
+++ src/presentation/video/video.presenter.ts
@@ -1,21 +1,39 @@
+import { IsDate, IsNumber, IsString } from 'class-validator';
+
 export class GetQuestionListPresenter {
+  @IsNumber()
   video_id: number;
+  @IsString()
   video_url: string;
+  @IsString()
   title: string;
+  @IsString()
   description: string;
+  @IsNumber()
   like_cnt: number;
+  @IsNumber()
   comment_cnt: number;
+  @IsDate()
   created_at: Date;
+  @IsString()
   profile: string;
+  @IsNumber()
   user_id: number;
 }
 
 export class GetVideoCommentListPresenter {
+  @IsNumber()
   video_id: number;
+  @IsString()
   video_url: string;
+  @IsString()
   title: string;
+  @IsDate()
   created_at: Date;
+  @IsNumber()
   like_cnt: number;
+  @IsString()
   profile: string;
+  @IsNumber()
   user_id: number;
 }
