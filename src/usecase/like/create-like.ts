f9a90aee86cb4ddc149a5b404e21b600fb49a94a
Seung jun <poodle2405@gmail.com>
2022-03-19 16:51:33 +0900

    :recycle: :: (#16) code repectoring

--- src/usecase/like/create-like.ts
+++ src/usecase/like/create-like.ts
@@ -1,10 +1,10 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, Scope } from '@nestjs/common';
 import { REQUEST } from '@nestjs/core';
 import { IException } from 'src/domain/exceptions/exceptions.interface';
 import { IUserReqeust } from 'src/domain/interfaces/request.interface';
 import { LikeRepository } from 'src/domain/repositories/like.repository';
 
-@Injectable()
+@Injectable({ scope: Scope.REQUEST })
 export class CreateLikeUsecase {
   constructor(
     private readonly likeRepository: LikeRepository,
@@ -16,7 +16,7 @@ export class CreateLikeUsecase {
   async execute(videoId: number) {
     const userId = this.request.user.userId;
     const checkTheLike = await this.likeRepository.findOne({ video_id: videoId, user_id: userId });
-    if (checkTheLike) this.exceptionsService.badRequestException({ message: 'Likes already exist', code_error: 400 });
+    if (checkTheLike) this.exceptionsService.likesAlreadyExistException();
 
     this.likeRepository.createLike(videoId, userId);
   }
