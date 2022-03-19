1b45b8a25cc23b9e49c8d64812728b6268c14200
Seung jun <poodle2405@gmail.com>
2022-03-19 18:27:38 +0900

    :recycle: :: (#16) repository repectoring

--- src/infrastructure/repositories/like.repository.ts
+++ src/infrastructure/repositories/like.repository.ts
@@ -3,33 +3,38 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { LikeRepository } from 'src/domain/repositories/like.repository';
 import { Repository } from 'typeorm';
 import { LikeTypeOrmEntity } from '../entities/like.entity';
+import { UserTypeOrmEntity } from '../entities/user.entity';
+import { VideoTypeOrmEntity } from '../entities/video.entity';
 
 @Injectable()
 export class DatabaseLikeRepository implements LikeRepository {
   constructor(
     @InjectRepository(LikeTypeOrmEntity)
     private readonly likeEntityRepository: Repository<LikeTypeOrmEntity>,
+
+    @InjectRepository(VideoTypeOrmEntity)
+    private readonly videoEntityRepository: Repository<VideoTypeOrmEntity>,
+
+    @InjectRepository(UserTypeOrmEntity)
+    private readonly userEntityRepository: Repository<UserTypeOrmEntity>,
   ) {}
 
   async createLike(videoId: number, userId: number): Promise<void> {
-    await this.likeEntityRepository
-      .createQueryBuilder('like')
-      .insert()
-      .into(LikeTypeOrmEntity)
-      .values({ user_id: userId, video_id: videoId })
-      .execute();
+    const user: UserTypeOrmEntity = await this.userEntityRepository.findOne(userId);
+    const video: VideoTypeOrmEntity = await this.videoEntityRepository.findOne(videoId);
+
+    await this.likeEntityRepository.save({
+      user,
+      video,
+    });
   }
 
-  async findOne(videoId: number, userId: number): Promise<boolean> {
-    const like = await this.likeEntityRepository
+  async findOne(videoId: number, userId: number) {
+    return await this.likeEntityRepository
       .createQueryBuilder('like')
       .select('like.id')
       .where('like.user_id = :user_id', { user_id: userId })
       .andWhere('like.video_id = :video_id', { video_id: videoId })
       .getOne();
-    if (like) {
-      return true;
-    }
-    return false;
   }
 }
