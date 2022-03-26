6968187758d7aae63821df5e72602b43b30a2366
Seung jun <poodle2405@gmail.com>
2022-03-26 15:33:00 +0900

    ⚡️ :: (#17) delete /like/

--- src/presentation/like/like.controller.ts
+++ src/presentation/like/like.controller.ts
@@ -1,13 +1,15 @@
-import { Body, Controller, Inject, Post, Scope, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Inject, Post, Scope, UseGuards } from '@nestjs/common';
 import { REQUEST } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
 import { IUserReqeust } from 'src/domain/interfaces/request.interface';
 import { CreateLikeUsecase } from 'src/usecase/like/create-like';
+import { DeleteLikeUsecase } from 'src/usecase/like/delete-like';
 
 @Controller({ path: '/like', scope: Scope.REQUEST })
 export class LikeController {
   constructor(
     @Inject(CreateLikeUsecase) private readonly createLikeUsecase: CreateLikeUsecase,
+    @Inject(DeleteLikeUsecase) private readonly deleteLikeUsecase: DeleteLikeUsecase,
     @Inject(REQUEST)
     private readonly request: IUserReqeust,
   ) {}
@@ -17,5 +19,12 @@ export class LikeController {
   async createLike(@Body('videoId') videoId: number) {
     const userId = this.request.user.userId;
     this.createLikeUsecase.execute(videoId, userId);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Delete('/')
+  async deleteLike(@Body('videoId') videoId: number) {
+    const userId = this.request.user.userId;
+    this.deleteLikeUsecase.execute(videoId, userId);
   }
 }
