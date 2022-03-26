d12cf590dfe7eef708f05c19885bec866cd79c7c
Seung jun <poodle2405@gmail.com>
2022-03-26 18:10:51 +0900

    :recycle: :: (#17) create-like

--- src/usecase/like/create-like.ts
+++ src/usecase/like/create-like.ts
@@ -1,4 +1,5 @@
 import { IException } from 'src/domain/exceptions/exceptions.interface';
+import { Like } from 'src/domain/model/like';
 import { User } from 'src/domain/model/user';
 import { Video } from 'src/domain/model/video';
 import { LikeRepository } from 'src/domain/repositories/like.repository';
@@ -7,16 +8,16 @@ import { VideoRepository } from 'src/domain/repositories/video.repository';
 
 export class CreateLikeUsecase {
   constructor(
-    private readonly exceptionsService: IException,
     private readonly likeRepository: LikeRepository,
     private readonly userRepository: UserRepository,
     private readonly videoRepository: VideoRepository,
+    private readonly exceptionsService: IException,
   ) {}
 
   async execute(videoId: number, userId: number) {
-    const checkTheLike = await this.likeRepository.findOne(videoId, userId);
+    const like: Like = await this.likeRepository.findOne(videoId, userId);
 
-    if (checkTheLike) this.exceptionsService.likesAlreadyExistException();
+    if (like) this.exceptionsService.likesAlreadyExistException();
 
     const user: User = await this.userRepository.findOne(userId);
     const video: Video = await this.videoRepository.findOne(videoId);
