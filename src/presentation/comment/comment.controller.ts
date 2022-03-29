c7bb5b10194ff21bd7a749f14734e1cf0a1c2903
Seung jun <poodle2405@gmail.com>
2022-03-29 09:13:38 +0900

    :recycle: :: (#9) code refactoring

--- src/presentation/comment/comment.controller.ts
+++ src/presentation/comment/comment.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Inject, Param, ParseIntPipe, Put } from '@nestjs/common';
+import { Controller, HttpCode, HttpStatus, Inject, Param, ParseIntPipe, Put } from '@nestjs/common';
 import { CommentAdoptionUsecase } from 'src/usecase/comment/comment-adoption';
 
 @Controller('/comment')
@@ -6,8 +6,8 @@ export class CommentController {
   constructor(@Inject(CommentAdoptionUsecase) private readonly commentAdoptionUsecase: CommentAdoptionUsecase) {}
 
   @Put('/adoption/:commentId')
+  @HttpCode(HttpStatus.OK)
   async videoAdoption(@Param('commentId', ParseIntPipe) commentId: number) {
     await this.commentAdoptionUsecase.execute(commentId);
-    return { status: 200, message: 'success' };
   }
 }
