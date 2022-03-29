9484b95adb96e01881760c90d59a7d7c32f24520
Seung jun <poodle2405@gmail.com>
2022-03-30 08:41:37 +0900

    ⚡️ :: (#39) user info 기능 추가

--- src/domain/repositories/user.repository.ts
+++ src/domain/repositories/user.repository.ts
@@ -1,3 +1,6 @@
+import { GetUserInfoPresenter } from './dto/user.dto';
+
 export interface UserRepository {
   findOne(userId: number);
+  userInfo(userId: number): Promise<GetUserInfoPresenter>;
 }
