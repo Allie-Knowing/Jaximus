b481e71d5f44b4c8add9a8c516fdede24c2db926
Seung jun <poodle2405@gmail.com>
2022-03-31 10:49:49 +0900

    :bookmark_tabs: :: (#45) answer presenter 생성

---
+++ src/presentation/answer/answer.presenter.ts
@@ -0,0 +1,18 @@
+import { IsDate, IsNumber, IsString } from 'class-validator';
+
+export class GetVideoAnswerListPresenter {
+  @IsNumber()
+  video_id: number;
+  @IsString()
+  video_url: string;
+  @IsString()
+  title: string;
+  @IsDate()
+  created_at: Date;
+  @IsNumber()
+  like_cnt: number;
+  @IsString()
+  profile: string;
+  @IsNumber()
+  user_id: number;
+}
