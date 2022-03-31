b2a92462d6fec14590834a0c3341fbb3e59b5adf
Seung jun <poodle2405@gmail.com>
2022-03-31 20:22:31 +0900

    ⚡️ :: (#8) change function

--- src/domain/repositories/comment.repository.ts
+++ src/domain/repositories/comment.repository.ts
@@ -2,5 +2,5 @@ export interface CommentRepository {
   findOne(commentId: number);
   commentAdoption(commentId: number): Promise<void>;
   deleteCommentAnswer(commentId: number): Promise<void>;
-  matchUser(userId: number);
+  findComment(commentId: number, userId: number);
 }
