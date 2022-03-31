d97b6178925ace61ed363f9a5a19a40aa5a31e2b
Seung jun <poodle2405@gmail.com>
2022-03-31 10:51:19 +0900

    :coffin: :: (#45) location change

--- src/presentation/video/video.controller.ts
+++ src/presentation/video/video.controller.ts
@@ -1,7 +1,5 @@
 import {
-  Body,
   Controller,
-  Get,
   Inject,
   Post,
   Param,
@@ -13,65 +11,21 @@ import {
   HttpStatus,
   HttpCode,
   UseGuards,
-  Delete,
 } from '@nestjs/common';
-import { Video } from 'src/domain/model/video';
-import { CreateVideoUsecase } from 'src/usecase/video/create-video';
-import { GetVideoCommentListPresenter } from './video.presenter';
-import { CreateVideoCommentUsecase } from 'src/usecase/video/create-video-comment';
 import { FileInterceptor } from '@nestjs/platform-express';
-import { GetQuestionListUseCases } from 'src/usecase/video/get-questions-list';
-import { GetVideoCommentListUseCases } from 'src/usecase/video/get-video-comment-list';
 import { VideoAdoptionUsecase } from 'src/usecase/video/video-adoption';
 import { IUserReqeust } from 'src/domain/interfaces/request.interface';
 import { REQUEST } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
-import { DeleteAnswerCommentUsecase } from 'src/usecase/video/delte-answer-comment';
 
 @Controller({ path: '/video', scope: Scope.REQUEST })
 export class VideoController {
   constructor(
-    @Inject(CreateVideoUsecase)
-    private readonly createVideoUsecase: CreateVideoUsecase,
-    @Inject(GetQuestionListUseCases)
-    private readonly getQuestionListUsecase: GetQuestionListUseCases,
-    @Inject(GetVideoCommentListUseCases)
-    private readonly getVideoCommentListUseCases: GetVideoCommentListUseCases,
-    @Inject(CreateVideoCommentUsecase)
-    private readonly createVideoCommentUsecase: CreateVideoCommentUsecase,
     @Inject(VideoAdoptionUsecase)
     private readonly videoAdoptionUsecase: VideoAdoptionUsecase,
-    @Inject(DeleteAnswerCommentUsecase)
-    private readonly deleteAnswerCommentUsecase: DeleteAnswerCommentUsecase,
     @Inject(REQUEST)
     private readonly request: IUserReqeust,
   ) {}
-
-  @UseGuards(AuthGuard('jwt'))
-  @Post('/')
-  @HttpCode(HttpStatus.CREATED)
-  create(@Body() request: Video) {
-    const userId = this.request.user.userId;
-    this.createVideoUsecase.execute(userId, request);
-  }
-
-  @Get('/')
-  questionList(): Promise<Video[]> {
-    return this.getQuestionListUsecase.execute();
-  }
-
-  @Get('/answer/:videoId')
-  videoAnswerList(@Param('videoId', ParseIntPipe) videoId: number): Promise<GetVideoCommentListPresenter[]> {
-    return this.getVideoCommentListUseCases.execute(videoId);
-  }
-
-  @UseGuards(AuthGuard('jwt'))
-  @Post('/answer')
-  @HttpCode(HttpStatus.CREATED)
-  async videoAnswer(@Body() request: Video) {
-    const userId = this.request.user.userId;
-    await this.createVideoCommentUsecase.execute(userId, request);
-  }
 
   @Post('/file')
   @UseInterceptors(FileInterceptor('file'))
@@ -79,17 +33,11 @@ export class VideoController {
     return { url: file.location };
   }
 
+  @UseGuards(AuthGuard('jwt'))
   @Put('/adoption/:videoId')
   @HttpCode(HttpStatus.OK)
   async videoAdoption(@Param('videoId', ParseIntPipe) videoId: number) {
-    await this.videoAdoptionUsecase.execute(videoId);
-  }
-
-  @UseGuards(AuthGuard('jwt'))
-  @Delete('/:commentId')
-  @HttpCode(HttpStatus.NO_CONTENT)
-  async delteVideoComment(@Param('commentId', ParseIntPipe) commentId: number) {
     const userId = this.request.user.userId;
-    await this.deleteAnswerCommentUsecase.execute(commentId, userId);
+    await this.videoAdoptionUsecase.execute(videoId, userId);
   }
 }
