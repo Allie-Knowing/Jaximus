4d0177acea398ecd613c673916a1ac3bd1accf03
leeseojune <sung07288346@gmail.com>
2022-04-05 02:13:57 +0900

    📑 :: query-title

---
+++ src/usecase/search/query-title.ts
@@ -0,0 +1,61 @@
+import { Client } from '@elastic/elasticsearch';
+import { ElasticsearchService } from 'src/infrastructure/config/elasticsearch/elasticsearch.service';
+
+export class QueryTitleUsecase {
+  client: Client;
+
+  constructor(private readonly elasticsearchService: ElasticsearchService) {}
+
+  async execute(q: string) {
+    function mapFunc(record: any) {
+      return {
+        id: record._id,
+        source: record._source,
+        highlight: record.highlight,
+      };
+    }
+
+    if (this.client === undefined) {
+      this.client = this.elasticsearchService.getESClient();
+    }
+
+    const result = await this.client.search({
+      index: 'videosearch',
+      body: {
+        query: {
+          bool: {
+            must: [
+              {
+                match: {
+                  'title.ngram': {
+                    query: q,
+                  },
+                },
+              },
+            ],
+            should: [
+              {
+                match: {
+                  'title.jaso': {
+                    query: q,
+                  },
+                },
+              },
+              {
+                match: {
+                  description: {
+                    query: q,
+                  },
+                },
+              },
+            ],
+          },
+        },
+      },
+    });
+
+    return result.body.hits.hits.map((record) => {
+      return mapFunc(record);
+    });
+  }
+}
