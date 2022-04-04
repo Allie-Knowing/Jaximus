5bfc1c3cbf325b1e9c2993da73454b9d40c07610
sonchaegeon <thscorjs1002@naver.com>
2022-04-04 19:03:04 +0900

    ⚡️ :: (#74) find question hashtag list query

--- src/infrastructure/repositories/hash-tag.repository.ts
+++ src/infrastructure/repositories/hash-tag.repository.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { HashTag } from 'src/domain/model/hash-tag';
 import { hashTagRepository } from 'src/domain/repositories/hash-tag.repository';
 import { Repository } from 'typeorm';
 import { HashTagTypeOrmEntity } from '../entities/hash-tag.entity';
@@ -10,4 +11,16 @@ export class DatabaseHashTagRepository implements hashTagRepository {
     @InjectRepository(HashTagTypeOrmEntity)
     private readonly hashTagEntityRepository: Repository<HashTagTypeOrmEntity>,
   ) {}
+
+  async findQuestionHashtagList(videoId: number): Promise<HashTag[]> {
+    const hashtags: any[] = await this.hashTagEntityRepository
+      .createQueryBuilder('hashtag')
+      .leftJoin('hashtag.question', 'video')
+      .select('hashtag.id', 'id')
+      .addSelect('hashtag.title', 'title')
+      .where('video.id = :videoId', { videoId })
+      .getRawMany();
+
+    return hashtags.map((hashtag) => new HashTag(hashtag));
+  }
 }
