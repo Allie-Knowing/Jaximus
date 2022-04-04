f5e3a61bc611614883bff834b9595e46ece7af26
Seung jun <poodle2405@gmail.com>
2022-04-04 22:06:25 +0900

    :recycle: :: code format

--- src/domain/model/user.ts
+++ src/domain/model/user.ts
@@ -2,17 +2,25 @@ import { Expose, Transform } from 'class-transformer';
 
 export class User {
   id: number;
+
   provider: string;
+
   email: string;
+
   profile: string;
+
   name: string;
+
   @Expose({ name: 'created_at' })
   createdAt: Date;
+
   @Expose({ name: 'updated_at' })
   updatedAt: Date;
+
   @Transform(({ value }) => parseInt(value))
   @Expose({ name: 'like_cnt' })
   likeCnt: number;
+
   @Transform(({ value }) => parseInt(value))
   @Expose({ name: 'video_cnt' })
   videoCnt: number;
