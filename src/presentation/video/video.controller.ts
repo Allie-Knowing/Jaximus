4d615d26016482c40fb679cafb6f33b2d47841ae
Seung jun <poodle2405@gmail.com>
2022-04-05 00:09:59 +0900

    ⚡️ :: (#79) add jwt guard

--- src/presentation/video/video.controller.ts
+++ src/presentation/video/video.controller.ts
@@ -1,10 +1,12 @@
-import { Controller, Post, Scope, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { Controller, Post, Scope, UploadedFile, UseGuards, UseInterceptors } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller({ path: '/video', scope: Scope.REQUEST })
 export class VideoController {
   constructor() {}
 
+  @UseGuards(AuthGuard('jwt'))
   @Post('/file')
   @UseInterceptors(FileInterceptor('file'))
   videoFile(@UploadedFile() file) {
