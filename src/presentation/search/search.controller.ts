ea251a499339352871be32e13aee1d849687d069
leeseojune <sung07288346@gmail.com>
2022-04-05 02:14:35 +0900

    ⚡ :: controller

--- src/presentation/search/search.controller.ts
+++ src/presentation/search/search.controller.ts
@@ -1,15 +1,31 @@
 import { Controller, Get, HttpCode, HttpStatus, Inject, Query, Scope } from '@nestjs/common';
-import { GetAutocompleteUsecase } from 'src/usecase/search/get-autocomplete';
+import { QueryAutocompleteUsecase } from 'src/usecase/search/query-autocomplete';
+import { QueryHashtagUsecase } from 'src/usecase/search/query-hash-tag';
+import { QueryTitleUsecase } from 'src/usecase/search/query-title';
 
 @Controller({ path: '/search', scope: Scope.REQUEST })
 export class SearchController {
   constructor(
-    @Inject(GetAutocompleteUsecase)
-    private readonly getAutocompleteUsecase: GetAutocompleteUsecase,
+    @Inject(QueryAutocompleteUsecase)
+    private readonly queryAutocompleteUsecase: QueryAutocompleteUsecase,
+    @Inject(QueryTitleUsecase)
+    private readonly queryTitleUsecase: QueryTitleUsecase,
+    @Inject(QueryHashtagUsecase)
+    private readonly queryHashTagUsecase: QueryHashtagUsecase,
   ) {}
 
   @Get('autocomplete')
   autocomplete(@Query('q') q: string) {
-    return this.getAutocompleteUsecase.execute(q);
+    return this.queryAutocompleteUsecase.execute(q);
+  }
+
+  @Get('title')
+  title(@Query('q') q: string) {
+    return this.queryTitleUsecase.execute(q);
+  }
+
+  @Get('hashtag')
+  hashtag(@Query('q') q: string) {
+    return this.queryHashTagUsecase.execute(q);
   }
 }
