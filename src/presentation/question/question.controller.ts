f01bfb3884f05c173a379b0a72689f69a7a0121a
sonchaegeon <thscorjs1002@naver.com>
2022-04-04 19:03:56 +0900

    ⚡️ :: (#74) question hashtag list controller

--- src/presentation/question/question.controller.ts
+++ src/presentation/question/question.controller.ts
@@ -19,6 +19,7 @@ import { IUserReqeust } from 'src/domain/interfaces/request.interface';
 import { Video } from 'src/domain/model/video';
 import { CreateQuestionUsecase } from 'src/usecase/question/create-question';
 import { DeleteQuestionUsecase } from 'src/usecase/question/delete-question';
+import { GetQuestionHashtagListUseCase } from 'src/usecase/question/get-question-hashtag-list';
 import { GetQuestionListUseCases } from 'src/usecase/question/get-questions-list';
 import { CreateQuestionDto } from './question.dto';
 
@@ -31,6 +32,8 @@ export class QuestionController {
     private readonly getQuestionListUsecase: GetQuestionListUseCases,
     @Inject(DeleteQuestionUsecase)
     private readonly deleteQuestionUsecase: DeleteQuestionUsecase,
+    @Inject(GetQuestionHashtagListUseCase)
+    private readonly getQuestionHashtagListUsecase: GetQuestionHashtagListUseCase,
     @Inject(REQUEST)
     private readonly request: IUserReqeust,
   ) {}
@@ -54,5 +57,10 @@ export class QuestionController {
   async deleteQuestion(@Param('videoId', ParseIntPipe) videoId: number) {
     const userId = this.request.user.sub;
     await this.deleteQuestionUsecase.execute(videoId, userId);
+  }
+
+  @Get('/:videoId/hashtag')
+  questionHashtagList(@Param('videoId', ParseIntPipe) videoId: number) {
+    return this.getQuestionHashtagListUsecase.execute(videoId);
   }
 }
