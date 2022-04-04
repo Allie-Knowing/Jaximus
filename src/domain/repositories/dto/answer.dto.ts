d3ef993a561b7bccca8050adaa15dfd587e4d100
Seung jun <poodle2405@gmail.com>
2022-04-04 12:06:11 +0900

    :coffin: :: (#71) delete domain class-validator

--- src/domain/repositories/dto/answer.dto.ts
+++ src/domain/repositories/dto/answer.dto.ts
@@ -1,11 +1,8 @@
 import { Expose } from 'class-transformer';
-import { IsString } from 'class-validator';
 
 export class CreateVideoAnswerDto {
-  @IsString()
   title: string;
 
-  @IsString()
   @Expose({ name: 'video_url' })
   videoUrl: string;
 }
--- src/domain/repositories/dto/question.dto.ts
+++ src/domain/repositories/dto/question.dto.ts
@@ -1,14 +1,10 @@
 import { Expose } from 'class-transformer';
-import { IsArray, IsString } from 'class-validator';
 
 export class CreateVideoDto {
-  @IsString()
   title: string;
 
-  @IsString()
   description: string;
 
-  @IsArray()
   @Expose({ name: 'hash_tag' })
   hashTags: string[];
 
