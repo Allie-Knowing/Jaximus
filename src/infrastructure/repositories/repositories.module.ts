401888324ac10be68c1ec44f30bcb51e23b18e3a
Seung jun <poodle2405@gmail.com>
2022-04-21 17:03:00 +0900

    ⚡️ :: (#130) iq Entity / Repository 주입

--- src/infrastructure/repositories/repositories.module.ts
+++ src/infrastructure/repositories/repositories.module.ts
@@ -3,11 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { TypeOrmConfigModule } from '../config/typeorm/typeorm.module';
 import { CommentTypeOrmEntity } from '../entities/comment.entity';
 import { HashTagTypeOrmEntity } from '../entities/hash-tag.entity';
+import { IqTypeOrmEntity } from '../entities/iq.entity';
 import { LikeTypeOrmEntity } from '../entities/like.entity';
 import { UserTypeOrmEntity } from '../entities/user.entity';
 import { VideoTypeOrmEntity } from '../entities/video.entity';
 import { DatabaseCommentRepository } from './comment.repository';
 import { DatabaseHashTagRepository } from './hash-tag.repository';
+import { DatabaseIqRepository } from './iq.repository';
 import { DatabaseLikeRepository } from './like.repository';
 import { DatabaseUserRepository } from './user.repository';
 import { DatabaseVideoRepository } from './video.repository';
@@ -15,7 +17,14 @@ import { DatabaseVideoRepository } from './video.repository';
 @Module({
   imports: [
     TypeOrmConfigModule,
-    TypeOrmModule.forFeature([UserTypeOrmEntity, CommentTypeOrmEntity, HashTagTypeOrmEntity, VideoTypeOrmEntity, LikeTypeOrmEntity]),
+    TypeOrmModule.forFeature([
+      UserTypeOrmEntity,
+      CommentTypeOrmEntity,
+      HashTagTypeOrmEntity,
+      VideoTypeOrmEntity,
+      LikeTypeOrmEntity,
+      IqTypeOrmEntity,
+    ]),
   ],
   providers: [
     DatabaseUserRepository,
@@ -23,7 +32,15 @@ import { DatabaseVideoRepository } from './video.repository';
     DatabaseHashTagRepository,
     DatabaseVideoRepository,
     DatabaseLikeRepository,
+    DatabaseIqRepository,
   ],
-  exports: [DatabaseUserRepository, DatabaseCommentRepository, DatabaseHashTagRepository, DatabaseVideoRepository, DatabaseLikeRepository],
+  exports: [
+    DatabaseUserRepository,
+    DatabaseCommentRepository,
+    DatabaseHashTagRepository,
+    DatabaseVideoRepository,
+    DatabaseLikeRepository,
+    DatabaseIqRepository,
+  ],
 })
 export class RepositoriesModule {}
