a545e1498b22de0afcd5835405103fd43433f134
Seung jun <poodle2405@gmail.com>
2022-04-21 17:00:35 +0900

    :recycle: :: (#130) hashTagRepository -> HashTagRepository

--- src/domain/repositories/hash-tag.repository.ts
+++ src/domain/repositories/hash-tag.repository.ts
@@ -1,5 +1,5 @@
 import { HashTag } from '../model/hash-tag';
 
-export interface hashTagRepository {
+export interface HashTagRepository {
   findQuestionHashtagList(videoId: number): Promise<HashTag[]>;
 }
