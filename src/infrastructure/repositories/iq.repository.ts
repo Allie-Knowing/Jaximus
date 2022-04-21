74eb6b1354be251ae06b1e1acd1c12c0ef647efd
Seung jun <poodle2405@gmail.com>
2022-04-21 17:02:25 +0900

    :bookmark_tabs: :: (#130) iq repository 생성

---
+++ src/infrastructure/repositories/iq.repository.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Iq } from 'src/domain/model/iq';
+import { IqRepository } from 'src/domain/repositories/iq.repository';
+import { Repository } from 'typeorm';
+import { IqTypeOrmEntity } from '../entities/iq.entity';
+
+@Injectable()
+export class DatabaseIqRepository implements IqRepository {
+  constructor(
+    @InjectRepository(IqTypeOrmEntity)
+    private readonly iqEntityRepository: Repository<IqTypeOrmEntity>,
+  ) {}
+
+  async getIq(userId: number): Promise<Iq> {
+    const iq = await this.iqEntityRepository
+      .createQueryBuilder('iq')
+      .select('iq.userId')
+      .addSelect('iq.curCnt')
+      .addSelect('iq.totCnt')
+      .where('iq.user_id = :user_id', { user_id: userId })
+      .getOne();
+
+    return new Iq(iq);
+  }
+
+  async iqExpenditure(userId: number, compensation: number): Promise<void> {
+    await this.iqEntityRepository
+      .createQueryBuilder()
+      .update(IqTypeOrmEntity)
+      .set({ curCnt: () => `cur_cnt - ${compensation}` })
+      .where('user_id = :user_id', { user_id: userId })
+      .execute();
+  }
+}
