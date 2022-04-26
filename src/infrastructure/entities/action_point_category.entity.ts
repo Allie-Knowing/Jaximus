b399432a3fa5a6b20e948db12b182fbb08721e93
Seung jun <poodle2405@gmail.com>
2022-04-26 10:07:06 +0900

    :bookmark_tabs: :: (#132) action point category entity 생성

---
+++ src/infrastructure/entities/action_point_category.entity.ts
@@ -0,0 +1,17 @@
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { ActionPointTypeOrmEntity } from './action_point.entity';
+
+@Entity('action_point_category')
+export class ActionPointCategoryTypeOrmEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ length: 20 })
+  name: string;
+
+  @Column({ nullable: true })
+  score: number;
+
+  @OneToMany(() => ActionPointTypeOrmEntity, (actionPoint) => actionPoint.actionPointCategory)
+  actionPoint: ActionPointTypeOrmEntity[];
+}
