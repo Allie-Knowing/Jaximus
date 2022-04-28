f3b4beb19aca3905b0930d97a60182405d5b01c5
sonchaegeon <thscorjs1002@naver.com>
2022-04-28 20:02:29 +0900

    ⚡️ :: available 200mb

--- src/infrastructure/config/multer/multer-config.service.ts
+++ src/infrastructure/config/multer/multer-config.service.ts
@@ -35,7 +35,7 @@ export class MulterConfigService implements MulterOptionsFactory {
           );
         },
       }),
-      limits: { fieldSize: 50 * 1024 * 1024 },
+      limits: { fieldSize: 200 * 1024 * 1024 },
     };
   }
 }
