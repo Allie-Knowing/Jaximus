cde269e15c6fd0ad7ee5330eef2873acb7d95894
sonchaegeon <thscorjs1002@naver.com>
2022-04-28 18:43:48 +0900

    ⚡️ :: (#122) tier model domain

--- src/domain/model/tier.ts
+++ src/domain/model/tier.ts
@@ -1,4 +1,4 @@
-import { Expose } from 'class-transformer';
+import { Expose, Transform } from 'class-transformer';
 
 export class Tier {
   @Expose({ name: 'user_id' })
@@ -9,4 +9,35 @@ export class Tier {
 
   @Expose({ name: 'updated_at' })
   updatedAt: Date;
+
+  @Expose({ name: 'category_name' })
+  categoryName: string;
+
+  @Transform(({ value }) => {
+    if (value) return parseInt(value);
+  })
+  @Expose({ name: 'cur_cnt' })
+  curCnt: number;
+
+  @Transform(({ value }) => {
+    if (value) return parseInt(value);
+  })
+  @Expose({ name: 'tot_cnt' })
+  totCnt: number;
+
+  @Expose({ name: 'next_tot_iq' })
+  nextTotIq: number;
+
+  @Expose({ name: 'next_adoption_cnt' })
+  nextAdoptionCnt: number;
+
+  @Expose({ name: 'image_url' })
+  imageUrl: string;
+
+  @Expose({ name: 'adoption_cnt' })
+  adoptionCnt: number;
+
+  constructor(obj) {
+    return Object.assign(this, obj);
+  }
 }
