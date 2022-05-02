a41aa92d8c08dab03854e3175ce1a796e8af94bb
sonchaegeon <thscorjs1002@naver.com>
2022-05-02 14:41:39 +0900

    ⚡️ :: (#145) action point calc query implemnt

---
+++ src/infrastructure/repositories/action-point.repository.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ActionPointRepository } from 'src/domain/repositories/action-point.repository';
+import { GetActionPointDto } from 'src/presentation/wallet/action-point.dto';
+import { Repository } from 'typeorm';
+import { ActionPointTypeOrmEntity } from '../entities/action-point.entity';
+
+@Injectable()
+export class DatabaseActionPointRepository implements ActionPointRepository {
+  constructor(
+    @InjectRepository(ActionPointTypeOrmEntity)
+    private readonly actionPointEntityRepository: Repository<ActionPointTypeOrmEntity>,
+  ) {}
+  async actionPointCalc(userId: number): Promise<GetActionPointDto> {
+    return await this.actionPointEntityRepository
+      .createQueryBuilder('action')
+      .select('SUM(category.score)', 'actionPoint')
+      .innerJoin('action.actionPointCategory', 'category')
+      .where('action.user = :userId', { userId })
+      .andWhere('action.created_at BETWEEN ADDDATE(now(), -1) AND now()')
+      .getRawOne();
+  }
+}
