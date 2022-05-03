f0853170b9fbf74aa87e9e7a9b7eaea926ce288e
sonchaegeon <thscorjs1002@naver.com>
2022-05-03 09:51:19 +0900

    ⚡️ :: (#138) like related action point

--- src/usecase/like/delete-like.ts
+++ src/usecase/like/delete-like.ts
@@ -1,9 +1,15 @@
 import { IException } from 'src/domain/exceptions/exceptions.interface';
 import { Like } from 'src/domain/model/like';
+import { CacheTemplate, generateCacheTemplate } from 'src/domain/enums/cache.enum';
 import { LikeRepository } from 'src/domain/repositories/like.repository';
+import { RedisCacheService } from 'src/infrastructure/config/redis/redis-cache.service';
 
 export class DeleteLikeUsecase {
-  constructor(private readonly likeRepository: LikeRepository, private readonly exceptionsService: IException) {}
+  constructor(
+    private readonly cacheService: RedisCacheService,
+    private readonly likeRepository: LikeRepository,
+    private readonly exceptionsService: IException,
+  ) {}
 
   async execute(videoId: number, userId: number) {
     const like: Like = await this.likeRepository.findOne(videoId, userId);
@@ -11,5 +17,6 @@ export class DeleteLikeUsecase {
     if (!like) this.exceptionsService.likeNotFoundException();
 
     this.likeRepository.deleteLike(videoId, userId);
+    this.cacheService.set(generateCacheTemplate(CacheTemplate.ACTION_LIKE, userId), -1);
   }
 }
