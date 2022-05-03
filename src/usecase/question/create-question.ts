8ba490c650ca72bbd11fceafb0988063dcee76c0
sonchaegeon <thscorjs1002@naver.com>
2022-05-03 09:50:37 +0900

    ⚡️ :: (#138) create question action point

--- src/usecase/question/create-question.ts
+++ src/usecase/question/create-question.ts
@@ -1,17 +1,24 @@
 import { Client } from '@elastic/elasticsearch';
+import { CacheTemplate, generateCacheTemplate } from 'src/domain/enums/cache.enum';
 import { IException } from 'src/domain/exceptions/exceptions.interface';
+import { ActionPointRepository } from 'src/domain/repositories/action-point.repository';
 import { IqRepository } from 'src/domain/repositories/iq.repository';
+import { UserRepository } from 'src/domain/repositories/user.repository';
 import { VideoRepository } from 'src/domain/repositories/video.repository';
 import { ElasticsearchService } from 'src/infrastructure/config/elasticsearch/elasticsearch.service';
+import { RedisCacheService } from 'src/infrastructure/config/redis/redis-cache.service';
 import { CreateQuestionDto } from 'src/presentation/question/question.dto';
 
 export class CreateQuestionUsecase {
   client: Client;
 
   constructor(
+    private readonly cacheService: RedisCacheService,
     private readonly videoRepository: VideoRepository,
     private readonly iqRepository: IqRepository,
     private readonly elasticsearchService: ElasticsearchService,
+    private readonly actionPointRepository: ActionPointRepository,
+    private readonly userRepository: UserRepository,
     private readonly exceptionsService: IException,
   ) {}
 
@@ -30,26 +37,28 @@ export class CreateQuestionUsecase {
     const splitVideoUrl = video.videoUrl.split('/');
     const videoFilename = splitVideoUrl[splitVideoUrl.length - 1];
     const splitVideoFilename = videoFilename.split('.');
-    this.client.index(
-      {
-        index: 'videosearch',
-        id: savedVideo.id.toString(),
-        type: '_doc',
-        body: {
-          doc: {
-            title: video.title,
-            description: video.description,
-            thumbnail: `https://test-knowing.s3.ap-northeast-2.amazonaws.com/processed/${splitVideoFilename[0] + 'knowing'}.${
-              splitVideoFilename[1]
-            }`,
-          },
+    this.client.index({
+      index: 'videosearch',
+      id: savedVideo.id.toString(),
+      type: '_doc',
+      body: {
+        doc: {
+          title: video.title,
+          description: video.description,
+          thumbnail: `https://test-knowing.s3.ap-northeast-2.amazonaws.com/processed/${splitVideoFilename[0] + 'knowing'}.${
+            splitVideoFilename[1]
+          }`,
         },
       },
-      (err, response) => {
-        console.log(err);
-        console.log('########');
-        console.log(response);
-      },
-    );
+    });
+
+    // action point
+    const questionTemplateKey = generateCacheTemplate(CacheTemplate.ACTION_CREATE_QUESTION, userId);
+    const actionCreateQuestion = await this.cacheService.get(questionTemplateKey);
+    if (!actionCreateQuestion) {
+      const user = await this.userRepository.findOne(userId);
+      await this.cacheService.setTtl(questionTemplateKey, 'x', 57600);
+      await this.actionPointRepository.saveActionPoint(user, 6);
+    }
   }
 }
