8d3e1e5fd977421b88aee003bce07378367264d5
sonchaegeon <thscorjs1002@naver.com>
2022-05-12 10:13:09 +0900

    ⚡️ :: expo push message for test logic

--- src/usecase/answer/create-text-answer.ts
+++ src/usecase/answer/create-text-answer.ts
@@ -1,9 +1,16 @@
 import { IException } from 'src/domain/exceptions/exceptions.interface';
 import { CommentRepository } from 'src/domain/repositories/comment.repository';
 import { VideoRepository } from 'src/domain/repositories/video.repository';
+import { Expo, ExpoPushMessage } from 'expo-server-sdk';
+import { ExpoService } from 'src/infrastructure/config/expo/expo.service';
+import { UserRepository } from 'src/domain/repositories/user.repository';
 
 export class CreateTextAnswerUsecase {
+  client: Expo;
+
   constructor(
+    private readonly expoService: ExpoService,
+    private readonly userRepository: UserRepository,
     private readonly commentRepository: CommentRepository,
     private readonly videoRepository: VideoRepository,
     private readonly exceptionsService: IException,
@@ -11,8 +18,34 @@ export class CreateTextAnswerUsecase {
 
   async execute(content: string, questionId: number, userId: number) {
     const question = await this.videoRepository.findOne(questionId);
+    const user = await this.userRepository.findOne(userId);
     if (!question) this.exceptionsService.questionNotFoundException();
 
     this.commentRepository.createCommentAnswer(content, questionId, userId);
+
+    if (this.client === undefined) {
+      this.client = this.expoService.getExpoServerClient();
+    }
+
+    if (!Expo.isExpoPushToken(user.expoToken)) {
+      console.error(`Push token ${user.expoToken} is not a valid Expo push token`);
+      return;
+    }
+    let messages: ExpoPushMessage[] = [
+      {
+        to: user.expoToken,
+        sound: 'default',
+        body: 'This is a test notification',
+      },
+    ];
+    let chunks = this.client.chunkPushNotifications(messages);
+    for (let chunk of chunks) {
+      try {
+        let ticketChunk = await this.client.sendPushNotificationsAsync(chunk);
+        console.log(ticketChunk);
+      } catch (error) {
+        console.error(error);
+      }
+    }
   }
 }
