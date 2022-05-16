a56be0b0b3356c06df2957a21461aa76abfdba1e
Seung jun <poodle2405@gmail.com>
2022-05-16 13:11:00 +0900

    ⚡️ :: (#156) Patch /video/views/:videoId 추가

--- src/presentation/video/video.controller.ts
+++ src/presentation/video/video.controller.ts
@@ -1,15 +1,24 @@
-import { Controller, Post, Scope, UploadedFile, UseGuards, UseInterceptors } from '@nestjs/common';
+import { Controller, Inject, Param, ParseIntPipe, Patch, Post, Scope, UploadedFile, UseGuards, UseInterceptors } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { VideoViewsUsecase } from 'src/usecase/video/video-views';
 
 @Controller({ path: '/video', scope: Scope.REQUEST })
 export class VideoController {
-  constructor() {}
+  constructor(
+    @Inject(VideoViewsUsecase)
+    private readonly videoViewsUsecase: VideoViewsUsecase,
+  ) {}
 
   @UseGuards(AuthGuard('jwt'))
   @Post('/file')
   @UseInterceptors(FileInterceptor('file'))
   videoFile(@UploadedFile() file) {
     return { url: file ? file.location : null };
+  }
+
+  @Patch('/views/:videoId')
+  videoViews(@Param('videoId', ParseIntPipe) videoId: number): Promise<void> {
+    return this.videoViewsUsecase.execute(videoId);
   }
 }
