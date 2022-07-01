e6dcea443726c902f72f5654b875bf0a0100272e
정지우 <jiwoourty@gmail.com>
2022-07-01 17:18:59 +0900

    :bug: :: modify cors option

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import { ResponseInterceptor } from './infrastructure/common/interceptors/respon
 import { LoggerService } from './infrastructure/logger/logger.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   app.useGlobalPipes(
     new ValidationPipe({
