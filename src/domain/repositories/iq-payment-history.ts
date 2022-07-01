e0012dfa1f35f1668d3cc9838861eb8b6b3246cf
sonchaegeon <thscorjs1002@naver.com>
2022-07-01 11:16:01 +0900

    ⚡️ :: (#168) upload event related methods
    question video event, answer video event, find by user id and payment category

--- src/domain/repositories/iq-payment-history.ts
+++ src/domain/repositories/iq-payment-history.ts
@@ -1,5 +1,11 @@
+import { IqPaymentCategoryTypeOrmEntity } from 'src/infrastructure/entities/iq-payment-category.entity';
+import { IqPaymentHistoryTypeOrmEntity } from 'src/infrastructure/entities/iq-payment-history.entity';
+import { UserTypeOrmEntity } from 'src/infrastructure/entities/user.entity';
 import { GetPaymentHistoryPresenter } from 'src/presentation/wallet/payment-history.presenter';
 
 export interface IqPaymentHistoryRepository {
+  questionVideoEvent(user: UserTypeOrmEntity, iqPaymentCategory: IqPaymentCategoryTypeOrmEntity): void;
+  answerVideoEvent(user: UserTypeOrmEntity, iqPaymentCategory: IqPaymentCategoryTypeOrmEntity): Promise<void>;
   getIqPaymentHistories(userId: number, page: number, size: number): Promise<GetPaymentHistoryPresenter[]>;
+  findByUserIdAndPaymentCategory(userId: number, categoryId: number): Promise<IqPaymentHistoryTypeOrmEntity>;
 }
