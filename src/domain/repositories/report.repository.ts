3d9f514a726dba1de14c93cad17dc575db074320
kwakdony <kwakdh25@gmail.com>
2022-07-07 14:12:01 +0900

    :zap: :: (#170) Fix query report list API

--- src/domain/repositories/report.repository.ts
+++ src/domain/repositories/report.repository.ts
@@ -1,7 +1,6 @@
 import { ReportTypeOrmEntity } from 'src/infrastructure/entities/report.entity';
-import { CreateCommentReportDto, CreateVideoReportDto } from 'src/presentation/report/report.dto';
+import { CreateCommentReportDto, CreateVideoReportDto, ReportResponseDto } from 'src/presentation/report/report.dto';
 import { Comment } from '../model/comment';
-import { Report } from '../model/report';
 import { User } from '../model/user';
 import { Video } from '../model/video';
 
@@ -9,6 +8,6 @@ export interface ReportRepository {
   findOne(reportId: number): Promise<ReportTypeOrmEntity>;
   saveVideoReport(dto: CreateVideoReportDto, user: User, video: Video): Promise<void>;
   saveCommentReport(dto: CreateCommentReportDto, user: User, video: Video, comment: Comment): Promise<void>;
-  findAll(): Promise<Report[]>;
+  findAll(): Promise<ReportResponseDto[]>;
   deleteReport(reportId: number): Promise<void>;
 }
--- src/presentation/report/report.dto.ts
+++ src/presentation/report/report.dto.ts
@@ -15,3 +15,29 @@ export class CreateCommentReportDto extends CreateVideoReportDto {
   @Expose({ name: 'comment_id' })
   commentId: number;
 }
+
+export class ReportResponseDto {
+  @Expose({ name: 'video_id' })
+  videoId: number;
+
+  @Expose({ name: 'user_id' })
+  userId: number;
+
+  @Expose({ name: 'report_id' })
+  reportId: number;
+
+  @Expose({ name: 'comment_id' })
+  commentId: number;
+
+  @Expose({ name: 'video_url' })
+  videoUrl: string;
+
+  description: string;
+
+  @Expose({ name: 'created_at' })
+  createdAt: Date;
+
+  constructor(obj) {
+    return Object.assign(this, obj);
+  }
+}
